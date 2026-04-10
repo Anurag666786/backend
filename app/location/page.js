eea70a8e04cabdb420ed975035959c5f1d930da5
Anurag666786 <itsanurag666@gmail.com>
2026-04-10 16:14:41 +0530

    Updated Location Map

---
+++ app/location/page.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Image from "next/image";
+
+const Page = () => {
+  return (
+    <main className="w-full h-screen">
+      <div className="relative w-full h-full">
+        {/* Background Image */}
+        <Image
+          src="/location.png"
+          alt="Location"
+          fill
+          className="object-cover"
+          priority
+        />
+      </div>
+    </main>
+  );
+};
+
+export default Page;
